60c8f09e369f06d9a9f43588412f04ef853076ee
さな <jonsana@team-lab.com>
2023-10-23 00:21:06 +0900

    upgrade: category colors

--- src/components/Category/constants.ts
+++ src/components/Category/constants.ts
@@ -1,12 +1,12 @@
 export const COLOR_SET: Record<string, string> = {
   0: "rgb(186 230 253)",
   1: "rgb(254 205 211)",
-  2: "rgb(245 208 254)",
-  3: "rgb(221 214 254)",
-  4: "rgb(191 219 254)",
-  5: "rgb(204 251 241)",
-  6: "rgb(187 247 208)",
-  7: "rgb(254 249 195)",
+  2: "rgb(218 218 218)", // grey
+  3: "rgb(234 231 247)", // light purple
+  4: "rgb(191 219 254)", // light blue
+  5: "rgb(212 242 221)", // light green
+  6: "rgb(175 198 170)", // dimmed green
+  7: "rgb(254 249 195)", // light yellow
   8: "rgb(186 230 253)",
   9: "rgb(254 202 202)",
   A: "rgb(231 229 228)",
