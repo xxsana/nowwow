8795ac5ba5140f57fb1325ed27d65ad89f4145e6
さな <jonsana@team-lab.com>
2023-10-22 23:20:16 +0900

    change profile image

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "nowwow",
-    image: "/fox-wink.PNG", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/profile.PNG", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "ios developer",
     bio: "",
     email: "sanajeong.dev@gmail.com",
